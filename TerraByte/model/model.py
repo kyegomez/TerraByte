761b7bd8c9dad199e19dfec519c2e461ee415690
Kye <kye@apacmediasolutions.com>
2023-08-03 19:53:51 -0400

    no attn bias

--- TerraByte/model/model.py
+++ TerraByte/model/model.py
@@ -264,6 +264,43 @@ class Attention(nn.Module):
 
 
 
+# class Transformer(nn.Module):
+#     def __init__(
+#         self,
+#         *,
+#         dim,
+#         layers,
+#         dim_head = 64,
+#         heads = 8,
+#         attn_dropout = 0.,
+#         ff_dropout = 0.,
+#         ff_mult = 4,
+#         rel_pos_bias = True,
+#         flash_attn = True,
+#     ):
+#         super().__init__()
+#         self.alibi = Alibi(heads = heads) if rel_pos_bias else None
+#         self.layers = nn.ModuleList([])
+
+#         for _ in range(layers):
+#             self.layers.append(nn.ModuleList([
+#                 Attention(dim = dim, dim_head = dim_head, heads = heads, dropout = attn_dropout, flash = flash_attn),
+#                 FeedForward(dim = dim, mult = ff_mult, dropout = ff_dropout)
+#             ]))
+
+#         self.norm = RMSNorm(dim)
+
+#     def forward(self, x):
+#         n = x.shape[-2]
+#         attn_bias = self.alibi(n, n, device = x.device) if exists(self.alibi) else None
+
+#         for attn, ff in self.layers:
+#             x = attn(token_shift(x), attn_bias = attn_bias) + x
+#             x = ff(token_shift(x)) + x
+
+#         return self.norm(x)
+    
+
 class Transformer(nn.Module):
     def __init__(
         self,
@@ -275,11 +312,11 @@ class Transformer(nn.Module):
         attn_dropout = 0.,
         ff_dropout = 0.,
         ff_mult = 4,
-        rel_pos_bias = True,
-        flash_attn = True,
+        rel_pos = True,
+        flash_attn = False
     ):
         super().__init__()
-        self.alibi = Alibi(heads = heads) if rel_pos_bias else None
+        self.rotary_emb = RotaryEmbedding(dim_head) if rel_pos else None
         self.layers = nn.ModuleList([])
 
         for _ in range(layers):
@@ -289,17 +326,6 @@ class Transformer(nn.Module):
             ]))
 
         self.norm = RMSNorm(dim)
-
-    def forward(self, x):
-        n = x.shape[-2]
-        attn_bias = self.alibi(n, n, device = x.device) if exists(self.alibi) else None
-
-        for attn, ff in self.layers:
-            x = attn(token_shift(x), attn_bias = attn_bias) + x
-            x = ff(token_shift(x)) + x
-
-        return self.norm(x)
-    
 
 
 

27883beaa29c135ba792ad0e581679fd73c83184
Kye <kye@apacmediasolutions.com>
2023-08-03 22:52:36 -0400

    clean up attention function

--- TerraByte/model/terrabyte_triton.py
+++ TerraByte/model/terrabyte_triton.py
@@ -66,18 +66,18 @@ class Attention(nn.Module):
         if exists(rotary_emb):
             q, k = map(lambda t: apply_rotary_pos_emb(rotary_emb, t), (q, k))
 
-        #ENSURE Q, K, V have atleast 4 dimensions
-        q = q.unsqueeze(0) if q.dim() < 4 else q
-        k = k.unsqueeze(0) if k.dim() < 4 else k
-        v = v.unsqueeze(0)if v.dim() < 4 else v
+        # #ENSURE Q, K, V have atleast 4 dimensions
+        # q = q.unsqueeze(0) if q.dim() < 4 else q
+        # k = k.unsqueeze(0) if k.dim() < 4 else k
+        # v = v.unsqueeze(0)if v.dim() < 4 else v
 
-        q.type(torch.float32)
-        k.type(torch.float32)
-        v.type(torch.float32)
+        # q.type(torch.float32)
+        # k.type(torch.float32)
+        # v.type(torch.float32)
 
-        # Ensure k has the correct shape
-        print(f'k shape: {k.shape}')
-        k = k.view(1, self.heads, -1, q.shape[-1])
+        # # Ensure k has the correct shape
+        # print(f'k shape: {k.shape}')
+        # k = k.view(1, self.heads, -1, q.shape[-1])
         out = self.attend(q, k, v, True, self.scale)  # Add causal and sm_scale parameters
 
         out = rearrange(out, 'b h n d -> b n (h d)')
